37e478f45cfc0200950fbbfd6916f1cea6407363
Lorenz Diener <lorenzd@gmail.com>
2019-05-10 01:10:30 +0200

    Initial attempt at a PyPi setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+test_deps = ['pytest', 'Pillow', 'numpy']
+extras = {
+      "test": test_deps
+}
+
+setup(name='blurhash',
+      version='1.0.0',
+      description='Pure-Python implementation of the blurhash algorithm.',
+      packages=['mastodon'],
+      install_requires=[],
+      tests_require=test_deps,
+      extras_require=extras,
+      url='https://github.com/halcy/blurhash-python',
+      author='Lorenz Diener',
+      author_email='lorenzd+blurhashpypi@gmail.com',
+      license='MIT',
+      keywords='blurhash graphics web_development',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Topic :: Multimedia :: Graphics',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ])
