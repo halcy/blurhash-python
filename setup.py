22e081ef1c24da1bb5c5eaa2c1d6649724deaef8
Lorenz Diener <lorenzd@gmail.com>
2019-10-11 20:54:41 +0200

    Switch setup.py back

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ extras = {
 }
 
 setup(name='blurhash',
-      version='1.1.3',
+      version='1.1.4',
       description='Pure-Python implementation of the blurhash algorithm.',
       packages=['blurhash'],
       install_requires=[],
@@ -15,14 +15,13 @@ setup(name='blurhash',
       url='https://github.com/halcy/blurhash-python',
       author='Lorenz Diener',
       author_email='lorenzd+blurhashpypi@gmail.com',
-      license='No-Nazis',
-      data_files = [("", ["LICENSE"])],
+      license='MIT',
       keywords='blurhash graphics web_development',
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Developers',
           'Topic :: Multimedia :: Graphics',
-          'License :: Other/Proprietary License',
+          'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 3',
       ])
